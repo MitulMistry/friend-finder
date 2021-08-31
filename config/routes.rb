2ddea4dd1d54df6800f5a2ca509b357c4167934c
Mitul Mistry <MitulMistryDev@gmail.com>
2021-08-30 23:24:38 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users do
+    resources :messages, only: [:new]
+  end
+
+  resources :messages, only: [:create, :destroy]
+
+  scope 'messages' do
+    get 'received', to: 'messages#receieved'
+    get 'sent', to: 'messages#sent'
+  end
+
+  resources :interests, only: [:index, :show]
 end
