50cb28ec1246d02ecc78c2da1cb2cad8e24759a9
Mitul Mistry <MitulMistryDev@gmail.com>
2021-09-02 14:48:02 -0500

    Add association and validation tests to model specs

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    # Using FactoryBot syntax methods in rails_helper.rb
+    expect(build(:message)).to be_valid
+  end
+
+  describe "associations" do
+    # Using shoulda-matchers
+    it { should belong_to(:sender) }
+    it { should belong_to(:recipient) }
+  end
+
+  describe "validations" do
+    context "required validations" do
+      it { should validate_presence_of(:body)}
+      it { should validate_length_of(:body).is_at_most(2000)}
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    # Using FactoryBot syntax methods in rails_helper.rb
+    expect(build(:user)).to be_valid
+  end
+
+  describe "associations" do
+    # Using shoulda-matchers
+    it { should have_many(:interests) }
+    it { should have_many(:sent_messages) }
+    it { should have_many(:received_messages) }
+  end
+
+  describe "validations" do
+    context "required validations" do
+      it { should validate_presense_of(:username) }
+      it { should validate_length_of(:username).is_at_least(3).is_at_most(40) }
+      it { should validate_uniqueness_of(:username) }
+      it { should have_secure_password }
+    end
+  end
+
+  describe "sort users" do
+    it "returns an array of all users sorted by creation date (newest first)"
+  end
+
+  describe "sort messages" do
+    it "returns an array of all sent messages sorted by creation date (newest first)"
+    
+    it "returns an array of all receieved messages sorted by creation date (newest first)"
+  end
 end
