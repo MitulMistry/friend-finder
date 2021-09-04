e17f046b3b07439865ca003ca3f6fe7fa64c502e
Mitul Mistry <MitulMistryDev@gmail.com>
2021-09-04 17:17:01 -0500

    Update routes for authentication and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :sessions, only: [:new, :create, :destroy]
-
-  resources :users do
+  resources :users, only: [:index, :show, :create, :edit, :update, :destroy] do
     resources :messages, only: [:new]
   end
+  get '/signup', to: 'users#new'
+
+  resources :sessions, only: [:create]
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :messages, only: [:create, :destroy]
 
@@ -15,4 +18,6 @@ Rails.application.routes.draw do
   end
 
   resources :interests, only: [:index, :show]
+
+  root 'pages#index'
 end
